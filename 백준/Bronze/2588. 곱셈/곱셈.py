41cde7a72d5a2d4474ca486307cb825b90008a10
yujin45 <113690378+yujin45@users.noreply.github.com>
2022-09-23 13:04:51 +0900

    [Bronze III] Title: 곱셈, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,9 @@
+n1 = input()
+n2 = input()
+n2_100 = int(n2)// 100
+n2_10 = int(n2) % 100 // 10
+n2_1 = int(n2) % 10
+print(int(n1)*n2_1 )
+print(int(n1)*n2_10)
+print(int(n1)*n2_100)
+print(int(n1)*int(n2))
